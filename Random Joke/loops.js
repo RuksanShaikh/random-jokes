02e27648d26cd9e8e37483eb01334e5e3b90b2cf
RuksanShaikh <146839713+RuksanShaikh@users.noreply.github.com>
2024-01-26 21:36:00 +0530

    Add files via upload

---
+++ Random Joke/loops.js
@@ -0,0 +1,16 @@
+let jokes=["i","k","a","s","l"];
+//console.log(jokes)
+
+// for (let index = 0; index < jokes.length; index++) {
+//     const element = jokes[index];
+//     console.log(element)
+// }
+
+// for (const i in jokes) {
+//     console.log(jokes[i])
+// }
+
+
+// for (const j of jokes) {
+//     console.log(j)
+// }
